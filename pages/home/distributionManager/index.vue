7b78b4ce221921a34b2b042307b38464925386cd
王博然 <br_0120@126.com>
2021-12-19 15:27:03 +0800

    更新编辑模式

--- pages/home/distributionManager/index.vue
+++ pages/home/distributionManager/index.vue
@@ -1,54 +1,24 @@
 <template>
-<div>
-    <p-nav 
-    :leftClick="leftClick"
-    />
-    <div class="ctn">
-        <div class="bg-img"></div>
-        <top />
-        <bottom />
-    </div>
-</div>
+	<div class="dm bg-w">
+		<p-nav title="分销管理"/>
+		<div class="profit">
+			您的收益分成：<span>¥0.20/页</span>
+		</div>
+	</div>
 </template>
-
 <script>
-import top    from './components/indexTop.vue'
-import bottom from './components/indexBottom.vue'
 export default {
-    components: { top, bottom },
-    onLaunch: function() {
-    },
-    onShow: function() {
-        
-    },
-    onHide: function() {
-        
-    },
-    methods: {
-        leftClick(e) {
-            console.log(e);
-        }
-    },
+		
 }
 </script>
 <style lang="scss" scoped>
-.ctn {
-    position: relative;
-    min-height: calc(734rpx + 288rpx);
-    max-width: 100vw;
-    .bg-img {
-        height: 1468rpx;
-        width: 1468rpx;
-        background: #605CD6;
-        background: -moz-linear-gradient(left, #2A91FE, #2A91FE 15%, #605CD6 85%, #605CD6);
-        background: -o-linear-gradient(left, #2A91FE, #2A91FE 15%, #605CD6 85%, #605CD6);
-        background: -webkit-gradient(linear, left top, right top, from(#2A91FE), color-stop(0.15, #2A91FE), color-stop(0.85, #605CD6), color-stop(0.75, #605CD6), to(#605CD6));
-        // background-image: linear-gradient(to right, #2A91FE , #605CD6);
-        border-radius: 50%;
-        position: absolute;
-        top: -734rpx;
-        left: calc(-734rpx + 50vw);
-        z-index: -1;
-    }
+.dm {
+	.profit {
+		height: 148rpx;
+		span {
+			font-size: 48rpx;
+			color: #FF8A00;
+		}
+	}
 }
 </style>
